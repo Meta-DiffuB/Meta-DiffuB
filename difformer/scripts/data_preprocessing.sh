adb955ebb6a72a79da9fc7061f77d8d1ebd42a9b
metabeta-diffusion <167835870+metabeta-diffusion@users.noreply.github.com>
2024-10-02 10:28:20 +0800

    Delete difformer directory

--- difformer/scripts/data_preprocessing.sh
+++
@@ -1,8 +0,0 @@
-fairseq-preprocess \
-    --source-lang en --target-lang de \
-    --trainpref wmt14.en-de.dist.bin/train \
-    --validpref wmt14.en-de.dist.bin/valid \
-    --testpref wmt14.en-de.dist.bin/test \
-    --destdir data-bin/wmt14_en_de \
-    --joined-dictionary \
-    --workers 20
